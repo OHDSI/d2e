6c8f262d84cb15b0e4afcf0520ea9966f7e43503
maggie-li-yd <maggie.li@data4life-asia.care>
2026-08-24 13:20:54 +0800

    resolve ts error and warning

--- plugins/atlas/subplugins/data-quality/src/env.d.ts
+++ plugins/atlas/subplugins/data-quality/src/env.d.ts
@@ -1,5 +1,17 @@
+/// <reference types="vite/client" />
+
 interface Window {
   System: {
     import: (id: string) => Promise<unknown>;
   };
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >;
+  export default component;
 }
