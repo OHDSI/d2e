501c22af5a24a2a84ab2eb07663f35c75974c6de
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-09-07 21:15:28 +0800

    Fall back to d2e-native concept search for non-webapi datasets

--- plugins/ui/apps/concept-sets/src/axios/api.ts
+++ plugins/ui/apps/concept-sets/src/axios/api.ts
@@ -1,10 +1,12 @@
 import { Terminology } from "./terminology";
+import { Portal } from "./portal";
 import { D2eWebapi } from "./d2e-webapi";
 import { Translation } from "./translation";
 import { PublicWebapiProxyAPI } from "./public-webapi-proxy";
 
 export const api = {
   terminology: new Terminology(),
+  portal: new Portal(),
   d2eWebapi: new D2eWebapi(),
   translation: new Translation(),
   publicWebapiProxyAPI: new PublicWebapiProxyAPI(),
